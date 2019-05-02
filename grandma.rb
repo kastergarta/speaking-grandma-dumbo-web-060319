7b39946e058d6bb9296901579edac9d3e94dcde4
Dennis <dennis.basyrov@gmail.com>
2019-05-02 04:08:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,13 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else phrase == phrase.upcase 
+    puts "NO, NOT SINCE 1938!"
+  end  
 end  
 
 
